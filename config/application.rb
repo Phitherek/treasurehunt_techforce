f9f265f3de7792cec72623752ad604113afd1089
Phitherek_ <phitherek@gmail.com>
2015-12-27 11:40:43 +0100

    Replaced test suite, did erb2haml.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module TreasurehuntTechforce
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+       g.test_suite :rspec
+    end
   end
 end
