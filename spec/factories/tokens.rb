94920a361b35b72fefd47c53895a1827071868a0
Phitherek_ <phitherek@gmail.com>
2015-12-28 15:37:23 +0100

    Integrated Shoulda Matchers with RSpec, User tests.

--- spec/factories/tokens.rb
+++ spec/factories/tokens.rb
@@ -1,6 +1,11 @@
+require 'forgery'
+
 FactoryGirl.define do
   factory :token do
-    
+      token { Forgery(:basic).text(exactly: 30)}
   end
 
+  factory :empty_token do
+      token nil
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to allow_value("test@test2.xyz").for(:email) }
+  it { is_expected.not_to allow_value("test").for(:email) }
+  it { is_expected.not_to allow_value("test@test2").for(:email) }
+  it { is_expected.to have_secure_password }
 end
