0b9b83502322f16a6b9c183fd8f37adf62599ee5
Phitherek_ <phitherek@gmail.com>
2015-12-28 21:33:39 +0100

    Automatic token generation on create.

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,4 +1,17 @@
 class Token < ActiveRecord::Base
     belongs_to :user
     validates :token, presence: true, uniqueness: true
+    before_validation :generate_token
+
+    private
+
+    def generate_token
+        if self.token.nil?
+            t = Forgery(:basic).text(exactly: 30)
+            while(!Token.where(token: t).empty?)
+                t = Forgery(:basic).text(exactly: 30)
+            end
+            self.token = t
+        end
+    end
 end
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Token, type: :model do
-  it { is_expected.to validate_presence_of(:token) }
   it { is_expected.to validate_uniqueness_of(:token) }
+  it "should generate a unique token on create" do
+      u = create(:user)
+      t = Token.create(user: u)
+      expect(t).not_to be_new_record
+      expect(t.token).not_to be_blank
+  end
+  it "should generate a unique token on save" do
+      u = create(:user)
+      t = Token.new
+      t.user = u
+      t.save!
+      expect(t.token).not_to be_blank
+  end
 end
